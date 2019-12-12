f22fb3035541e35ce95d7555d4c46f33c47c6c84
abuyako <abuyako@gmail.com>
2019-12-12 15:26:16 +0300

    declaring the function and the variables for calculate tios

---
+++ SimpleCalculator/tipcalculator.js
@@ -0,0 +1,6 @@
+//Calculate tips
+function CalculateTip() {
+    var billAmt = document.getElementById("billamt").value;
+    var serviceQual = document.getElementById("serviceQual").value;
+    var numOfPeople = document.getElementById("peopleamt").value;
+}
