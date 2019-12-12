b9aa6a12a7a368a8331d0e16bc555a0f390ad6b4
abuyako <abuyako@gmail.com>
2019-12-12 10:39:05 +0300

    Declaring the date, second variable and styling the degree that it will rotate

--- ClockWithJS/script.js
+++ ClockWithJS/script.js
@@ -3,5 +3,11 @@ const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
+    const now = new Date();
     
+    const second = now.getSeconds();
+    const secondDegrees = ((second / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    Console.log(secondDegrees)
+
 }
