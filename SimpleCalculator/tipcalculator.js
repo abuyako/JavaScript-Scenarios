f9f645224f3d459b71e521041e28504171f4fe8e
abuyako <abuyako@gmail.com>
2019-12-12 15:28:54 +0300

    Put the conditional statement for checking the input

--- SimpleCalculator/tipcalculator.js
+++ SimpleCalculator/tipcalculator.js
@@ -3,4 +3,10 @@ function CalculateTip() {
     var billAmt = document.getElementById("billamt").value;
     var serviceQual = document.getElementById("serviceQual").value;
     var numOfPeople = document.getElementById("peopleamt").value;
+
+    //Validate input 
+    if (billAmt === "" || serviceQual == 0) {
+        alert("Pleade enter the value");
+        return;
+    }
 }
