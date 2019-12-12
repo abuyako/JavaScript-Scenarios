38adb2e98d880f92e4abb873b378effaed1eda2f
abuyako <abuyako@gmail.com>
2019-12-12 15:35:42 +0300

    checking to see if the number of people in the input in empty or less than or equal to 1

--- SimpleCalculator/tipcalculator.js
+++ SimpleCalculator/tipcalculator.js
@@ -9,4 +9,12 @@ function CalculateTip() {
         alert("Pleade enter the value");
         return;
     }
+
+    //check to see if this input is empty or less or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById("each").style.display = "none";
+    }else {
+        document.getElementById("each").style.display = "block";
+    }
 }
