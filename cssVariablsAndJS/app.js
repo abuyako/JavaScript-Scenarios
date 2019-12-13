73a6f9edf071507eb982e14e701fec51ea38fb49
abuyako <abuyako@gmail.com>
2019-12-13 11:40:44 +0300

    add the foreach in the input which we add addEventalisterner

--- cssVariablsAndJS/app.js
+++ cssVariablsAndJS/app.js
@@ -3,6 +3,7 @@ const inputs = document.querySelectorAll('.container inputs');
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
-
-    
 }
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
