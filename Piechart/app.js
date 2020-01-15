6676677629e1daa0d4f5d1062cbf049beb168f1d
abuyako <abuyako@gmail.com>
2020-01-15 15:25:30 +0300

    Draws the chart and set the chart values

--- Piechart/app.js
+++ Piechart/app.js
@@ -1,2 +1,14 @@
-google.charts.load('current',{'packages':['corechart']});
+google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Task", "Hours per Day"],
+    ["Work", 8],
+    ["Eat", 2],
+    ["TV", 4],
+    ["Gym", 2],
+    ["Sleep", 8]
+  ]);
+}
+``
