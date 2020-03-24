e4f8cca59825a06c3701e314881f949d83c461e0
abuyako <abuyako@gmail.com>
2020-03-24 10:10:02 +0300

    declaring the items

---
+++ change-backgroung-color/app.js
@@ -0,0 +1,4 @@
+const button = document.querySelector('button');
+const body   = document.querySelector('body');
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'black', 'tomato'];
+
