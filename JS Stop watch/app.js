743d78ac3d30cddda593c5dcfcfaf1c6c33fa1b9
abuyako <abuyako@gmail.com>
2020-04-20 15:09:38 +0300

    adding function with statment inside for sec, min and hours

--- JS Stop watch/app.js
+++ JS Stop watch/app.js
@@ -1,6 +1,18 @@
-var h1 = document.getElementsByTagName('h1')[0],
+var h1 = document.getElementsByTagName('h2')[0],
     start = document.getElementById('start'),
     stop = document.getElementById('stop'),
     clear = document.getElementById('clear'),
     seconds = 0, minutes = 0, hours = 0,
     t;
+
+    function add() {
+        seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes >= 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
