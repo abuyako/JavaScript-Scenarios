4af998c35c7193e0ccd87b118b5497ae5aacffb4
abuyako <abuyako@gmail.com>
2020-04-21 14:47:29 +0300

    adding the short hand for the if statement

--- JS Stop watch/app.js
+++ JS Stop watch/app.js
@@ -15,4 +15,7 @@ var h1 = document.getElementsByTagName('h2')[0],
                 hours++;
             }
         }
-    }
+            h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ':' + 
+                            (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + 
+                            (seconds > 9 ? seconds : "0" + seconds);
+        }
