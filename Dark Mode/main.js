76c375121190f26e1b3ea48c9b1cd4fdfe1da692
abuyako <abuyako@gmail.com>
2020-04-30 20:37:20 +0300

    add more concept

--- Dark Mode/main.js
+++ Dark Mode/main.js
@@ -1,9 +1,33 @@
-function toggleTheme (theme) {
-    const body = document.body;
-    body.classList.remove('dark', 'light')
-    if (theme == 'dark') {
-        body.classList.add('dark') 
-    } else {
-        body.classList.add('light')
-    } 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
