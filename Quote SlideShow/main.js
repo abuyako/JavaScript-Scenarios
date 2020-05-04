a14ed0734f2187b4e935261a88a3612f34aaa361
abuyako <abuyako@gmail.com>
2020-05-04 15:13:40 +0300

    adding function for the slides

--- Quote SlideShow/main.js
+++ Quote SlideShow/main.js
@@ -9,3 +9,10 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    
+}
+
