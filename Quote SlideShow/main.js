ec3f634da511266ec84509d69c50d241608a030f
abuyako <abuyako@gmail.com>
2020-05-04 15:18:28 +0300

    adding loops for the slides

--- Quote SlideShow/main.js
+++ Quote SlideShow/main.js
@@ -18,5 +18,11 @@ function showSlides(n) {
     } if (n < 1) {
         slideIndex = slides.length;
     }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].getElementsByClassName.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
 }
 
