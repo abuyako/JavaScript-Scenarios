00319cad336042974a411e248609c042af59dcf6
abuyako <abuyako@gmail.com>
2020-05-04 15:15:27 +0300

    adding statement for the slides

--- Quote SlideShow/main.js
+++ Quote SlideShow/main.js
@@ -13,6 +13,10 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    
+    if (n > slides.length) {
+        slideIndex = 1;
+    } if (n < 1) {
+        slideIndex = slides.length;
+    }
 }
 
